763937a5b95e41c7b964142ffea3fc5bb9bf2d65
Gamze Şirin <gamze1sirin@gmail.com>
2024-05-02 18:24:24 +0300

    nextjs project workspace created.

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Header from '@/components/header/Header'
+import React from 'react'
+
+export default function Home() {
+	return (
+		<React.Fragment>
+			<Header />
+		</React.Fragment>
+	)
+}
