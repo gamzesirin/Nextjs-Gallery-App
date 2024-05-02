8fb45acdc789c345fafdc25d7c3d9a1f65206244
Gamze Şirin <gamze1sirin@gmail.com>
2024-05-02 22:26:16 +0300

    added.

--- app/page.js
+++ app/page.js
@@ -1,10 +1,15 @@
+import Banner from '@/components/banner/Banner'
 import Header from '@/components/header/Header'
+import Main from '@/components/main/Main'
 import React from 'react'
-
+import Section from '@/components/section/Section'
 export default function Home() {
 	return (
 		<React.Fragment>
 			<Header />
+			<Banner />
+			<Section />
+			<Main />
 		</React.Fragment>
 	)
 }
