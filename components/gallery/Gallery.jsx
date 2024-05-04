08acbd891efd8f2656dcb38c96903413ddd3276d
Gamze Şirin <gamze1sirin@gmail.com>
2024-05-04 17:39:42 +0300

    gallery updated.

--- components/gallery/Gallery.jsx
+++ components/gallery/Gallery.jsx
@@ -3,31 +3,20 @@ import React from 'react'
 
 const Gallery = () => {
 	return (
-		<div className="flex felx-col items-center justify-center mt-50">
-			<div className="mt-64 flex items-center justify-center">
-				<div className="flex gap-12 p-5">
-					<div className="w-96">
+		<div className="flex felx-col  mt-50 pt-5">
+			<div className="mt-64 flex items-center justify-center ">
+				<div className="flex gap-64 ml-[530px] ">
+					<div className="w-[36rem]">
 						<h1 className="text-2xl font-semibold">Create your online gallery</h1>
 						<p>You can create your own gallery with your own images and share it with your friends.</p>
-						<p></p>
+						<p>
+							You can create your own gallery with your own images and share it with your friends. You can create your
+							own gallery with your own images and share it with your friends.
+						</p>
 					</div>
 					<div>
-						<Image src="/gallery.svg" width={400} height={400} alt="gallery"></Image>
+						<Image src="/gallery.svg" width={450} height={450} alt="gallery"></Image>
 					</div>
-				</div>
-			</div>
-			<div className="flex flex-row">
-				<div className="basis-1/4">
-					<Image src="/a1.svg" width={100} height={100} alt="card1"></Image>
-				</div>
-				<div className="basis-1/4">
-					<Image src="/a2.svg" width={100} height={100} alt="card2"></Image>
-				</div>
-				<div className="basis-1/4">
-					<Image src="/a3.svg" width={100} height={100} alt="card3"></Image>
-				</div>
-				<div className="basis-1/4">
-					<Image src="/a4.svg" width={100} height={100} alt="card4"></Image>
 				</div>
 			</div>
 		</div>
