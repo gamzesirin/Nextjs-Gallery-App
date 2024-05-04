5b2148f253ba365328c5f1f30c7dc0bc482a922d
Gamze Şirin <gamze1sirin@gmail.com>
2024-05-04 19:16:17 +0300

    added.

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
+import '@fortawesome/fontawesome-svg-core/styles.css'
+
 import Banner from '@/components/banner/Banner'
 import Bottombanner from '@/components/bottombanner/Bottombanner'
+import Bottomsection from '@/components/bottomsection/Bottomsection'
+import Footer from '@/components/footer/Footer'
 import Gallery from '@/components/gallery/Gallery'
 import Header from '@/components/header/Header'
 import Main from '@/components/main/Main'
 import React from 'react'
 import Section from '@/components/section/Section'
-
 export default function Home() {
 	return (
 		<React.Fragment>
@@ -15,6 +18,8 @@ export default function Home() {
 			<Main />
 			<Gallery />
 			<Bottombanner />
+			<Bottomsection />
+			<Footer />
 		</React.Fragment>
 	)
 }
